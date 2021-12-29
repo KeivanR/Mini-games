8b4c91c2ab929a2b63d7bef61dddddd4bf0c689f
Keivan RAZBAN <34062925+KeivanR@users.noreply.github.com>
2021-12-29 10:47:41 +0000

    GRU + crossentropy
    eg [0,1,0], with softmax

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 moves = ['p','c','f']
-learning_rate = .0001
+learning_rate = .01
 timestep = 16
+batchsize = 10
 # Instantiate an optimizer.
 optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate)
 def loss(y_truth,y_pred):
-    return (y_pred-y_truth)**2
+    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_pred,y_truth))#(y_pred-y_truth)**2
 # Given a callable model, inputs, outputs, and a learning rate...
 def train(model, x, y_truth, learning_rate):
 
@@ -24,49 +25,57 @@ def train(model, x, y_truth, learning_rate):
     # Update the weights of the model.
     optimizer.apply_gradients(zip(dweights, model.trainable_weights))
     return current_loss
-    
+def int2vec(x):
+    return tf.cast(tf.equal(tf.range(3),x),tf.float32)
 
 ####CREATE MODEL###
 model = tf.keras.Sequential()
-model.add(tf.keras.Input(shape=(timestep,2,)))
+model.add(tf.keras.Input(shape=(timestep,6,)))
 model.add(layers.GRU(16))
 #model.add(tf.keras.Input(shape=(timestep*2,)))
 #model.add(layers.Dense(8,activation='tanh'))
-model.add(layers.Dense(1))
+model.add(layers.Dense(3,activation = 'softmax'))
 
 model.summary()    
 
 
 
-x = tf.zeros((1,timestep,2))
 auto = True
-y=0
-y_pred = [[0]]
-k=0
+x = tf.zeros((batchsize,timestep,6))
+y=tf.zeros((batchsize,3))
+y_pred=tf.zeros((batchsize,3))
+ylast = 0
+number_of_moves = 10000
 accuracy = 0
-pause_size = 500
-while k<100000:
-    k +=1
+pause_size = 50
+for move in range(number_of_moves):
     if not auto:
-        y = input('Chi-Fou-Mi!')
-        y = tf.cast(tf.where(tf.equal(moves,y))[0,0],tf.float32)
+        ylast = input('Chi-Fou-Mi!')
+        ylast = tf.cast(tf.where(tf.equal(moves,ylast))[0,0],tf.int32)
     else:
-        #y+=1
-        #y = y%3
-        y = (int(tf.math.round(y_pred))-1)%3
+        #ylast+=1
+        #ylast = ylast%3
+        ylast = (int(tf.math.round(y_pred[-1,0]))-1)%3
+    ylast = int2vec((ylast-1)%3)
+    y = tf.concat((y[1:],[ylast]),0)
     y_pred = model(x)
-    current_loss = train(model, x, (y-1)%3, learning_rate)
-    x = tf.concat((x[:,1:],[[[y_pred[0,0],y]]]),1)
-    accuracy += moves[int((y-1)%3)]==moves[int(tf.math.round(y_pred))]
-    if k%pause_size==0:
-            print(int(100*accuracy/pause_size),'% of computer wins')
-    if k%pause_size==0 or not auto:
-        print('User ' ,moves[int(y)])
-        print('Computer: ',moves[int(tf.math.round(y_pred))])
+
+    accuracy += tf.cast(tf.argmax(y[-1])==tf.argmax(y_pred[-1]),tf.float32)
+    
+    current_loss = train(model, x, y, learning_rate)
+    
+    xlast = tf.concat((x[-1,1:],[tf.concat((y_pred[-1],y[-1]),0)]),0)
+    x = tf.concat((x[1:],[xlast]),0)
+    if move%pause_size==0:
+        print(int(100*accuracy/pause_size),'% of computer wins')
+    if move%pause_size==0 or not auto:
+        print('User ' ,moves[(tf.argmax(y[-1])+1)%3])
+        print('Computer: ',moves[tf.argmax(y_pred[-1])])
         
-        print('ypred' ,y_pred)
+        print('ypred' ,y_pred[-1])
         print('LOSS: ',current_loss)
         if 100*(accuracy/pause_size)>90:
-            ans = input('If you want manual, press m') 
+            ans = input('If you want manual, press m\n') 
             auto = ans!='m'
         accuracy = 0
+
